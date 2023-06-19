bc666e4f32ed22c16abf1917733920a75c444721
Kapish Verma <122527683+kapishverma@users.noreply.github.com>
2023-06-19 17:24:16 +0530

    Add files via upload

---
+++ React Props/src/components/ListItem.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+
+// Complete this Component
+const ListItem = (props) => {
+
+  const { id, name, link, icon, bgColor } = props.data;
+  return (
+    <div key={id}
+      className="ListItem"
+      style={{
+        height: 30,
+        backgroundColor: bgColor
+}}
+    >
+      <img src={icon} alt={name} />
+      <a href={link}>{name}</a>
+    </div >
+  );
+};
+
+export default ListItem;
