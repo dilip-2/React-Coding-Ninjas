de6e6d8d87db479babe7e7ec55079b36025622d9
Kapish Verma <122527683+kapishverma@users.noreply.github.com>
2023-07-14 15:07:07 +0530

    Add files via upload

---
+++ NFTium/src/App.js
@@ -0,0 +1,13 @@
+import "./styles.css";
+import { Component } from "react";
+
+export default function App() {
+
+  return (
+    <div className="App">
+      <Navbar />
+      <Hero />
+      <About />
+    </div>
+  );
+}
